8c741722aa3b0f9a98c49d198fa89166a50f4fd5
ibrahimsenan <ibrahim.senan@yahoo.com>
2020-01-20 16:08:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom'
-import Routes from './components/Routes'
+import Routes from './components/Routes/Routes'
 import {HistoryView} from './components'
 
 

7cd737b0f85e48be8238710424312b5091bafc79
ibrahimsenan <ibrahim.senan@yahoo.com>
2020-01-20 14:22:45 +0100

    Create Redux Store

---
+++ src/store.js
@@ -0,0 +1,21 @@
+//Global Store Create
+
+import { applyMiddleware, createStore, compose ,combineReducers } from 'redux';
+import thunk from "redux-thunk";
+import reducers from './reducers/job_market_reducer';
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+);
+const combineAppReducers = combineReducers({state: reducers});
+const store = createStore(combineAppReducers,  enhancer);
+
+
+export default store;

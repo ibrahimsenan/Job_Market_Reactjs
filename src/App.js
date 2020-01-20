b11ccc98eeda60b1ebec821e84fbe53270c66551
ibrahimsenan <ibrahim.senan@yahoo.com>
2020-01-20 14:23:20 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom'
+import Routes from './components/Routes'
+import {HistoryView} from './components'
+
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("APP", this.props);
+
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <div className="headerTop">
+                        <div className="headerTitel">
+                            <img className={"imageStyle"} src={"./logo192.png"}/>
+                            Recruitment Portal
+                        </div>
+                    </div>
+                </header>
+                <body className="App-body">
+                <Routes/>
+                {this.props.children}
+                </body>
+
+            </div>
+        );
+    }
+}

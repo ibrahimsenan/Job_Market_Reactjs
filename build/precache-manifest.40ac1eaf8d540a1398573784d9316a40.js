393635f1d5152d9d9935e8dc08621f898b34088c
ibrahimsenan <ibrahim.senan@yahoo.com>
2020-01-20 14:15:16 +0100

    Initialize Job Market Project

---
+++ build/precache-manifest.40ac1eaf8d540a1398573784d9316a40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01402349e6016e8a8f6ea2d242d9e1ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06fb73f195ae5fc616f1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8810e87ea94526c4f3e3",
+    "url": "/static/js/2.f4cd2087.chunk.js"
+  },
+  {
+    "revision": "06fb73f195ae5fc616f1",
+    "url": "/static/js/main.dc93e4df.chunk.js"
+  },
+  {
+    "revision": "fdfcaeb42f5004cad39a",
+    "url": "/static/js/runtime~main.9ba1da3e.js"
+  }
+]);

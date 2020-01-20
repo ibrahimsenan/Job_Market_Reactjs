225d9d291bcf90fc6a0c56147805c4b97293d43f
ibrahimsenan <ibrahim.senan@yahoo.com>
2020-01-20 14:24:20 +0100

    Create Component Index file
    file contain all compnents index

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import JobTableView from "./views/JobTableView";
+import JobTableComponent from "./views/JobTableComponent";
+import InputView from "./views/InputsView"
+import TextAreaView from "./views/TextAreaView"
+import Routes from "./Routes"
+import Modal from "./views/Modal"
+import HistoryView from "./views/HistoryView"
+
+
+export {JobTableView, Routes, InputView, JobTableComponent, TextAreaView, Modal, HistoryView}
+export default {JobTableView, Routes, InputView, JobTableComponent, TextAreaView, Modal, HistoryView}
+

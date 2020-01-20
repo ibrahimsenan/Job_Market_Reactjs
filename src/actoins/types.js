e884c8b1a5343da68c1728f188afeb1d14782daf
ibrahimsenan <ibrahim.senan@yahoo.com>
2020-01-20 14:22:25 +0100

    Create Redux Types

---
+++ src/actoins/types.js
@@ -0,0 +1,23 @@
+//***global actions***
+
+// Get JobDataSet Collection Values
+export const GET_Collection_DataSet_PENDING = 'get_collection_dateset_pending';
+export const GET_Collection_DataSet_FULFILLED = 'get_collection_dateset_fulfilled';
+export const GET_Collection_DataSet_REJECTED = 'get_collection_dateset_rejected';
+
+// POST JobDataSet Collection Values
+export const POST_Collection_DataSet_PENDING = 'post_collection_dateset_pending';
+export const POST_Collection_DataSet_FULFILLED = 'post_collection_dateset_fulfilled';
+export const POST_Collection_DataSet_REJECTED = 'post_collection_dateset_rejected';
+
+// UPDATE JobDataSet Collection Values
+export const PUT_Collection_DataSet_PENDING = 'put_collection_dateset_pending';
+export const PUT_Collection_DataSet_FULFILLED = 'put_collection_dateset_fulfilled';
+export const PUT_Collection_DataSet_REJECTED = 'put_collection_dateset_rejected';
+
+// DELETE JobDataSet Collection Values
+export const DELETE_Collection_DataSet_PENDING = 'delete_collection_dateset_pending';
+export const DELETE_Collection_DataSet_FULFILLED = 'delete_collection_dateset_fulfilled';
+export const DELETE_Collection_DataSet_REJECTED = 'delete_collection_dateset_rejected';
+// Change State
+export const Change_View_State = 'change_View_State_Pending';
